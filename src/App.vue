c654b49ba9d66a10b84b8661764ca1cf8c8f2763
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-14 20:57:19 +0300

    add second_section, mobile adaptation

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div>
     <my-header/>
     <first-section/>
+    <second-section/>
   </div>
 </template>
 
 <script>
 import MyHeader from "@/components/MyHeader.vue";
 import FirstSection from "@/components/FirstSection.vue";
+import SecondSection from "@/components/SecondSection.vue";
 
 export default {
   name: "App",
-  components: {FirstSection, MyHeader}
+  components: {SecondSection, FirstSection, MyHeader}
 }
 </script>
 
---
+++ src/components/SecondSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="second_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="d-flex align-items-center">
+            <img src="../assets/img/second_section/star.svg" alt="star" class="mw-100">
+            <span class="star">since 1973</span>
+          </div>
+          <h2>All suites have a unique design<br>because we want our every guest<br>to feel special.</h2>
+          <h3>Premier Standard</h3>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 d-flex">
+          <p>The Superior twin would perfectly match<br>
+            the needs of those who plan to stay long.<br>
+            The bright and airy room is equipped with<br>
+            superior soft Italian furniture. Large<br>
+            windows open a beautiful view to the<br>
+            historical part of the city. Contemporary<br>
+            interior design and comfortable beds will<br>
+            make your stay cozy and delightful.</p>
+        </div>
+        <div class="second_image">
+          <img src="../assets/img/second_section/second_room.png" alt="room" class="mw-100">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SecondSection"
+}
+</script>
+
+<style scoped lang="scss">
+@import "src/assets/styles/second_section";
+
+</style>
