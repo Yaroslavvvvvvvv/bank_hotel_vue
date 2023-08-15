e3b311c9168e1dd1d8aad89ba51f6cd1fea5d0b6
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-15 15:56:35 +0300

    do mobile adaptation in third sextion

--- src/components/ThirdSection.vue
+++ src/components/ThirdSection.vue
@@ -35,6 +35,18 @@
         <div class="col-lg-6 col-md-6 col-sm-6">
           <a class="third_location" href="#">8 Lystopadovoho<br>Chynu,Lviv</a>
         </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 d-flex align-items-center">
+          <span class="next_header">Get in</span>
+          <img src="../assets/img/room/third_section/third_star.svg" alt="star" class="third_logo">
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 d-flex pt-4">
+          <div class="position-relative">
+            <img src="../assets/img/room/third_section/sofa.png" alt="sofa" class="mw-100">
+            <div class="position-absolute">
+              <span class="last_header">touch</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
